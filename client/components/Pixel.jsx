e12611fa0e5ceba90643f22462ffdeb2f27661e2
runminty <zengzhenhuang@gmail.com>
2019-04-09 11:54:17 +1200

    Add Rainbow coloring

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,31 +1,54 @@
 import React from 'react'
 
-class Pixel extends React.Component{
 
-  constructor(props){
+
+class Pixel extends React.Component {
+
+  constructor(props) {
+    const randomHexColor = () =>
+      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
     super(props)
-    this.state ={
-       style:{
-        height:"50px",
-        width:"50px",
-        backgroundColor:"cornflowerblue"
-        }
-    
-        
-    }
-//this.setState ({style})
-    }
-       
-  
-    render() {
-      const {style} =this.state
-  
 
-      return(
-        <div style={style}/>
-   
-      )
+    this.state = {
+      style: {
+        height: "50px",
+        width: "50px",
+        backgroundColor: randomHexColor()
+      }
 
-     }
+    }
+    }
+    changeColour = () => {
+      const style = {
+        width,
+        height,
+        backgroundColor: randomColour()
+      }
+  
+      this.setState({
+        style
+      })
+  }
+
+
+  render() {
+
+
+
+
+    const { style } = this.state
+
+
+    return (
+      <div
+      onClick={() => this.changeColour()}
+      onMouseEnter={() => this.changeColour()}
+      style={style} ></div>
+    )
+  }
 }
+
+
+
 export default Pixel
