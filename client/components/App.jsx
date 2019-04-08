29d8f333beb4703c593b0bf344237232e7bca45d
SharleneRyan <sa.ryan@live.com>
2019-04-09 11:55:10 +1200

    add pixel class

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 
+import Pixel from './Pixel.jsx'
+
 const App = () => {
   return (
-    <div>React development has begun!</div>
+    <div>
+      {/* /* <div style={{
+          fontFamily: 'Times New Roman',
+          fontSize: 30,
+          height: 100,
+          weight: 100,
+          backgroundColor: 'lightYellow'
+          // color: 'red'
+      }}> */ }
+        <br />
+        
+        {/* <div class='doto'>
+          <img src="/images/stage0.jpg" alt="picture"/>
+        </div> */}
+      
+    <Pixel />
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+    <Pixel />      
+
+
+    </div>
   )
 }
 
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Pixel  extends React.component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  } 
+  render () {
+    <div>
+      <div style={{
+        backgroundColor: 'lightblue',
+        width: 100,
+        height: 65,
+      }}>
+        {/* <img src="/images/stage1.jpg" alt="picture"/> */}
+        <div></div>
+      </div>
+    </div>
+  }
+}
+
+export default Pixel
