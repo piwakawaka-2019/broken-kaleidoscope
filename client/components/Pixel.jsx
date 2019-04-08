fe1821ed1d1c64aca8907381b06c602a1af4c4a3
deswhelan <wheland6@tcd.ie>
2019-04-09 10:49:49 +1200

    define Pixel class

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 
-const Pixel = () => {
-  return (
-    <div style={{
-        height: '1em',
-        width: '1em',
-        backgroundColor: 'green'    
-    }}></div>
-  )
+// const Pixel = () => {
+//   return (
+//     <div style={{
+        // height: '1em',
+        // width: '1em',
+        // backgroundColor: 'green'    
+//     }}></div>
+//   )
+// }
+
+class Pixel extends React.Component {
+    constructor(props){
+        super(props)
+        console.log("Props: ", this.props)
+        this.state = {
+            style: {
+                height: '1em',
+                width: '1em',
+                backgroundColor: 'green'
+            }
+        }
+    }
+
+    render () {
+        return (
+            <div style={this.state.style}></div>
+        )
+    }
+
 }
 
 export default Pixel
