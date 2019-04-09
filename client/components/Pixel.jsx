55050a1befbc44caca1b24f5e9eba83cf0db0f41
runminty <zengzhenhuang@gmail.com>
2019-04-09 15:54:08 +1200

    Its working

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,29 +1,81 @@
 import React from 'react'
 
-
+const randomHexColor = () =>
+    `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 class Pixel extends React.Component {
 
   constructor(props) {
-    const randomHexColor = () =>
-      `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
+    
     super(props)
 
     this.state = {
       style: {
-        height: "50px",
-        width: "50px",
+        height: "25px",
+        width: "25px",
         backgroundColor: randomHexColor()
       }
+    }
+    }
+
+
+    changeColourToGreen = () =>{
+      const style = {
+        height: "25px",
+        width: "25px",
+        backgroundColor: 'white'
+      }
+  
+      this.setState({
+        style
+      })
 
     }
+
+
+    changeColourToYellow = () =>{
+      const style = {
+        height: "25px",
+        width: "25px",
+        backgroundColor: 'black'
+      }
+  
+      this.setState({
+        style
+      })
+
+    }
+
+    changeColourToWhite = () =>{
+      const style = {
+        height: "25px",
+        width: "25px",
+        backgroundColor: 'white'
+      }
+  
+      this.setState({
+        style
+      })
+
+    }
+
+    changeColourToPink = () =>{
+      const style = {
+        height: "25px",
+        width: "25px",
+        backgroundColor: 'pink'
+      }
+  
+      this.setState({
+        style
+      })
+
     }
     changeColour = () => {
       const style = {
-        width,
-        height,
-        backgroundColor: randomColour()
+        height: "25px",
+        width: "25px",
+        backgroundColor: randomHexColor()
       }
   
       this.setState({
@@ -33,17 +85,15 @@ class Pixel extends React.Component {
 
 
   render() {
-
-
-
-
     const { style } = this.state
-
-
     return (
       <div
       onClick={() => this.changeColour()}
-      onMouseEnter={() => this.changeColour()}
+      onMouseEnter={() => this.changeColourToGreen()}
+      onDragEnter= {()=> this.changeColourToYellow()}
+
+      onDoubleClick={()=>this.changeColourToWhite()}
+      onContextMenu={()=>this.changeColourToPink()}
       style={style} ></div>
     )
   }
