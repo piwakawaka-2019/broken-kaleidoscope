956a3e994342f231aa9da38bb596ef98c05ee0eb
SharleneRyan <sa.ryan@live.com>
2019-04-09 18:00:43 +1200

    tied

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,13 +3,7 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
-  // let Pixel =[];
-  // for(let i=0;1 <1000;1++){
-  //  Pixel[i]=<Pixel /> ={i}/>
-  // }
-  //   return(
-  //     <Pixel/>
-  //   )
+  
   return Array.from({ length: 10000 }, () => <Pixel />)
 }
 
