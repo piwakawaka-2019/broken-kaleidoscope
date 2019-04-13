013d2a8bd4b3a01b12f34ec5ff19878247624559
Devon Lumsden <devonlumsden@mail.com>
2019-04-13 18:11:06 +1200

    add event handlers

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -12,10 +12,71 @@ export default class Pixel extends React.Component {
         }
     }
 
+    clickHandler = evt => {
+        this.setState({
+            style: {
+                height: '200px',
+                width: '200px',
+                backgroundColor: this.randomHexColor()
+            }
+        })
+    }
+
+    mouseEnterHandler = evt => {
+        this.setState({
+            style: {
+                height: '200px',
+                width: '200px',
+                backgroundColor: this.randomHexColor()
+            }
+        })
+    }
+
+    rightClickHandler = evt => {
+        this.setState({
+            style: {
+                height: '100px',
+                width: '100px',
+                backgroundColor: 'black'
+            }
+        })
+        event.preventDefault()
+    }
+
+    doubleClickHandler = evt => {
+        this.setState({
+            style: {
+                height: '200px',
+                width: '200px',
+                backgroundColor: 'white'
+            }
+        })
+    }
+
+    // dragEnterHandler = evt => {
+    //     this.setState({
+    //         style: {
+    //             height: '200px',
+    //             width: '200px',
+    //             backgroundColor: 'yellow'
+    //         }
+    //     })
+    // }
+
     randomHexColor = () => 
         `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
     render () {
-        return <div style={this.state.style}></div>
+        return (
+        <div 
+        style={this.state.style} 
+        onClick={this.clickHandler} 
+        onMouseEnter={this.mouseEnterHandler}
+        onContextMenu={this.rightClickHandler}
+        onDoubleClick={this.doubleClickHandler}
+        onDragEnter={this.dragEnterHandler}
+        >
+        </div>
+        )
     }
 }
